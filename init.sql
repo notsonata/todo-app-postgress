4328b20734cd56971d0831c6b3541c25eddf4dfa
sonata <botyok2005@gmail.com>
2025-02-07 23:25:37 +0800

    another bug fix

--- init.sql
+++ init.sql
@@ -7,7 +7,8 @@ CREATE TABLE IF NOT EXISTS task (
 -- Insert initial data
 INSERT INTO task (task_id, task, status) VALUES
     (1, 'Read an article on React.js', 'Done'),
-    (2, 'Organize a meeting', 'Pending');
+    (2, 'Organize a meeting', 'Pending')
+ON CONFLICT (task_id) DO NOTHING;
 
 -- Reset the sequence to continue from our last insert
 SELECT setval('task_task_id_seq', 3, false);
